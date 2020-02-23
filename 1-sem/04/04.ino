79e25d36eed7f170134f61583017d0a344d4dd40
Matheus Acosta <matheus.martins@qflash.com.br>
2020-02-23 16:10:59 -0300

    Sugestao de solucao aula 4.

--- 1-sem/04/04.ino
+++ 1-sem/04/04.ino
@@ -1,27 +1,23 @@
-/*
-  Use barra e asterisco para fazer comentários com mais de uma linha
-  mas sempre se lembre, um código bem escrito muitas vezes não precisa
-  de comentários.
-  Lembrem sempre de dar nomes que façam sentido as coisas durante seu código,
-  e evite linhas muito longas por dificultar para ler o código.
-*/
+#define LED 9
 
-// Use duas barras para fazer um comentário de uma linha
-
-// A função setup que faz a configuração inicial antes do loop principal
 void setup() {
-  // Inicializa o LED_BUILTIN do Arduino como um pino de saida(OUTPUT)
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);   // liga o led
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
 }
 
-// Função principal que faz um loop infinito
 void loop() {
-  if (LED_BUILTIN == HIGH) {
-    digitalWrite(LED_BUILTIN, LOW);
-  } else {
-    digitalWrite(LED_BUILTIN, HIGH);   // liga o led
-  }    // desliga o led
-
-  delay(1000);                       // espera um segundo
+  if (Serial.available() > 0) {
+    char caractere;
+    caractere = Serial.read();
+    if (caractere != '\n') {
+      Serial.println(caractere);
+      if (String(caractere) == "H") {
+        digitalWrite(LED, HIGH);
+      } else if (String(caractere) == "L") {
+        digitalWrite(LED, LOW);
+      }
+      delay(10);
+    }
+  }
+  delay(100);
 }
