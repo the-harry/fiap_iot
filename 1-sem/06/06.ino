d17735f526ffed26b94275cd9cda1294e2fd3f41
Matheus Acosta <matheus.martins@qflash.com.br>
2020-02-24 16:08:57 -0300

    aula 6

---
+++ 1-sem/06/06.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+
+#define PIR 7
+#define SERVO 9
+#define INTERVAL 1000
+
+Servo servo;
+
+int pos;
+int movimento = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(PIR, INPUT);
+  servo.attach(SERVO);
+  servo.write(180);
+}
+
+void loop() {
+  if (digitalRead(PIR) == HIGH) {
+    Serial.println("Alimentando o pet.");
+
+    for (pos = 180; pos >= 0; pos--) {
+      servo.write(pos);
+      delay(15);
+    }
+
+    delay(300);
+
+    for (pos = 0; pos < 180; pos++) {
+      servo.write(pos);
+      delay(15);
+    }
+  }
+  else {
+    Serial.println("Nenhum miau...");
+  }
+  delay(1000)
+}
